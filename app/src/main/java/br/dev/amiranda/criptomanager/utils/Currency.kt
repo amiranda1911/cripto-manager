16a8d1135f9e968494357ee9ded3c11a9f94a51d
Anderson Miranda <anderson.js.mi@outlook.com>
2024-07-26 12:59:50 -0300

    add Currency icon field

--- app/src/main/java/br/dev/amiranda/criptomanager/utils/Currency.kt
+++ app/src/main/java/br/dev/amiranda/criptomanager/utils/Currency.kt
@@ -2,7 +2,8 @@ package br.dev.amiranda.criptomanager.utils
 
 data class Currency (
     val symbol: String,
+    val icon: String,
     val name: String,
     val type: String,
-    val category : Category
+    val category : Category?
 )
