e47c742cfae18a1c70b35352f4640c274d2b40ab
Anderson Miranda <anderson.js.mi@outlook.com>
2024-07-30 23:40:20 -0300

    correct best variable

--- app/src/main/java/br/dev/amiranda/criptomanager/domain/MarketData.kt
+++ app/src/main/java/br/dev/amiranda/criptomanager/domain/MarketData.kt
@@ -6,14 +6,13 @@ import java.math.BigDecimal
 data class MarketData (
     @SerializedName("market_symbol")
     val symbol: String,
+    @SerializedName("last_trade")
     val lastTrade: Trade,
-
+    @SerializedName("rolling_24h")
     //last 24h
     val rolling24h: Rolling24h,
 
-    //best prices
-    val ask: Trade,
-    val bid: Trade
+    val best: Best
 
 )
 
