72592513694542d97ae95e67959d3f5d3371393a
Anderson Miranda <anderson.js.mi@outlook.com>
2024-07-30 23:41:16 -0300

    remove date

--- app/src/main/java/br/dev/amiranda/criptomanager/domain/Trade.kt
+++ app/src/main/java/br/dev/amiranda/criptomanager/domain/Trade.kt
@@ -1,11 +1,8 @@
 package br.dev.amiranda.criptomanager.domain
 
-import com.google.gson.annotations.SerializedName
 import java.math.BigDecimal
-import java.util.Date
 
 data class Trade(
     val price: BigDecimal,
     val volume: BigDecimal,
-    val date: Date
 )
