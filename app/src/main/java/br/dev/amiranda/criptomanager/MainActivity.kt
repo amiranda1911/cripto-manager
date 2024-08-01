06c7ac5dd716e722875a67466b3b9fdb36b1a47a
Anderson Miranda <anderson.js.mi@outlook.com>
2024-07-31 21:35:17 -0300

    add fragment manager

--- app/src/main/java/br/dev/amiranda/criptomanager/MainActivity.kt
+++ app/src/main/java/br/dev/amiranda/criptomanager/MainActivity.kt
@@ -22,5 +22,11 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, CurrenciesFragment())
+                .commit()
+        }
     }
 }
