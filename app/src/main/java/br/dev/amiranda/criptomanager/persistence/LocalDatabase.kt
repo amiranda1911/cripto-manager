6e7bad0670f082a1420a5700359856d279340652
Anderson Miranda <anderson.js.mi@outlook.com>
2024-08-04 17:31:13 -0300

    add wallet functions to database

--- app/src/main/java/br/dev/amiranda/criptomanager/persistence/LocalDatabase.kt
+++ app/src/main/java/br/dev/amiranda/criptomanager/persistence/LocalDatabase.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import br.dev.amiranda.criptomanager.ResourceRequest
+import br.dev.amiranda.criptomanager.domain.Wallet
+import java.math.BigDecimal
 
 class LocalDatabase(context: Context){
 
@@ -28,15 +30,47 @@ class LocalDatabase(context: Context){
         val cursor = db.query("icons",null,"symbol = \"$symbol\"", null,null, null, null )
         if(cursor.moveToFirst()){
             svg = cursor.getString(cursor.getColumnIndex("svg"))
+            cursor.close()
             return svg
         }
         else{
             svg = ResourceRequest().run() { getIconFromApi(symbol) }
             addIcon(symbol, svg)
         }
-
         //Foxbit app limits 6 requests per second
-
         return svg
     }
+
+    fun addWallet(wallet: Wallet) {
+        val db = dbHelper.writableDatabase
+        val newWallet = ContentValues().apply{
+            put("symbol", wallet.symbol)
+            put("value", wallet.value.toDouble())
+            put("buy_value", wallet.buyValue.toDouble())
+        }
+        db.insert("wallets", null, newWallet)
+        db.close()
+    }
+
+    @SuppressLint("Range")
+    fun getWallets(symbol: String) : List<Wallet> {
+        val db = dbHelper.writableDatabase
+        var wallets: List<Wallet> = listOf()
+
+        val cursor = db.query("wallets",null,"symbol = \"$symbol\"", null,null, null, null )
+        cursor.use{
+            while(cursor.moveToNext()) {
+                var wallet = Wallet(
+                    cursor.getString(cursor.getColumnIndex("symbol")),
+                    BigDecimal.valueOf(cursor.getDouble(cursor.getColumnIndex("value"))),
+                    BigDecimal.valueOf(cursor.getDouble(cursor.getColumnIndex("buy_value"))),
+                    BigDecimal.valueOf(cursor.getDouble(cursor.getColumnIndex("sell_value")))
+                    )
+                wallets = wallets + wallet
+            }
+
+        }
+        return wallets
+    }
+
 }
--- app/src/main/java/br/dev/amiranda/criptomanager/persistence/LocalDatabaseHelper.kt
+++ app/src/main/java/br/dev/amiranda/criptomanager/persistence/LocalDatabaseHelper.kt
@@ -6,17 +6,26 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class LocalDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "storage", null, 1) {
 
-    private val createSQL = "CREATE TABLE icons " +
+    private val createIconsTable = "CREATE TABLE icons " +
             "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
             "symbol TEXT NOT NULL, " +
             "svg TEXT )"
 
+    private val createWalletTable = "CREATE TABLE wallets" +
+            "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "symbol TEXT NOT NULL," +
+            "value REAL NOT NULL," +
+            "buy_value REAL NOT NULL," +
+            "sell_value REAL)"
+
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(createSQL)
+        db.execSQL(createIconsTable)
+        db.execSQL(createWalletTable)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
         db.execSQL("DROP TABLE IF EXISTS icons")
+        db.execSQL("DROP TABLE IF EXISTS wallets")
         onCreate(db)
     }
     
