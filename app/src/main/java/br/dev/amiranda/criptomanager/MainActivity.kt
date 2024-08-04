a66801382b5086a79b813cb1691a81229ba9b716
Anderson Miranda <anderson.js.mi@outlook.com>
2024-08-04 17:35:42 -0300

    remove old code

--- app/src/main/java/br/dev/amiranda/criptomanager/MainActivity.kt
+++ app/src/main/java/br/dev/amiranda/criptomanager/MainActivity.kt
@@ -1,19 +1,9 @@
 package br.dev.amiranda.criptomanager
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import br.dev.amiranda.criptomanager.persistence.LocalDatabase
-import br.dev.amiranda.criptomanager.utils.Currency
-import br.dev.amiranda.criptomanager.viewadapter.CurrencyViewAdapter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
+import br.dev.amiranda.criptomanager.fragments.CurrenciesFragment
 
 class MainActivity : FragmentActivity() {
 
